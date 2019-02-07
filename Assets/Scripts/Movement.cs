5f40ff075cbf25eb751a4ef50edc4fcef23e1e43
MissingBytes <vidyadheeshadn98@gmail.com>
2019-02-07 14:56:07 +0530

    Created a cart with basic movt

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    Rigidbody cart;
+	// Use this for initialization
+	void Start () {
+        cart = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        cart.velocity = new Vector3(h*3, 0, v*3);
+		
+	}
+}
