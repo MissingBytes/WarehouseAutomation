3182ff47c6ea582a80a65948e22af3fd972b2d7b
MissingBytes <vidyadheeshadn98@gmail.com>
2019-02-08 16:18:57 +0530

    Added object lifting

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
     Rigidbody cart;
+    Transform Package;
+    bool cart_loaded = false;
 	// Use this for initialization
 	void Start () {
         cart = GetComponent<Rigidbody>();
@@ -16,6 +18,19 @@ public class Movement : MonoBehaviour {
         float v = Input.GetAxis("Vertical");
 
         cart.velocity = new Vector3(h*3, 0, v*3);
+        if (!cart_loaded)
+        {
+            Collider[] nearby_object = Physics.OverlapSphere(cart.transform.position, 1.5f, 1 << 8);
+            if (nearby_object.Length != 0)
+                //for (int i = 0; i < nearby_object.Length; i++)
+                {
+                    nearby_object[0].GetComponent<Rigidbody>().useGravity = false;
+                    nearby_object[0].transform.position = cart.transform.position + new Vector3(0, 0.8f, 0);
+                    nearby_object[0].transform.parent = cart.transform;
+                    Debug.Log(nearby_object[0].name);
+                    cart_loaded = true;
+                }
+        }
 		
 	}
 }
